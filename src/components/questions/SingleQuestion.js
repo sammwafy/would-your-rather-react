7ed6ceeddecbb7ee91203f4b0165434dfb7a6512
sammwafy <59634544+sammwafy@users.noreply.github.com>
2021-12-19 19:29:13 +0200

    Update SingleQuestion.js
    changed the logic to redirect to 404 page only for the newly added questions ( that doesn't exist on the database file and exist only in the redux store )

--- src/components/questions/SingleQuestion.js
+++ src/components/questions/SingleQuestion.js
@@ -4,53 +4,39 @@ import { Navigate, useLocation } from "react-router";
 import { AnswerQuestion } from "./newAnswer";
 import { QuestionResults } from "./questionResults";
 import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router";
+
 export const SingleQuestion = () => {
   const questions_list = useSelector((state) => state.questions);
-  const location = useLocation();
-  const authUser = useSelector((state) => state.authUser);
-  const [ShowResults, setShowResults] = useState(false)
+  const authUser = useSelector((state) => state.authUser[0]);
+  const [ShowResults, setShowResults] = useState(false);
   const param = useParams();
-const navigate= useNavigate();
   const questionID = param.question_id;
-  let question = Object.values(questions_list).filter(
-    (q) => q.id === questionID
-  )[0];
+  const question = questions_list[questionID];
+  const location = useLocation();
 
-// set the dependency of useEffect on [location]
-// as the state changes after answer is submitted
-// then we show the results by setting value true to
-// the boolean ShowResults
-
-// if question has been answered already
-let alreadyAnswered = "";
-if(location.state !== null) {
-  alreadyAnswered= location.state.state
-}
-if (location.state === null){
-  navigate('/notfound')
-}
-useEffect(() => {
-  if(location.state !== null &&  'showResults' in location.state){
-
-    setShowResults(true)
+  // check if the question has been answered
+  let alreadyAnswered = "";
+  if (location.state !== null) {
+    alreadyAnswered = location.state.state;
+  }
+  if (location.state === null) {
+    <Navigate to="/notfound" />;
   }
 
-  // if user access question directly then navigate to 404 error page
-  else if(location.state === null){
-    navigate('/notfound')
-  }
- 
-}, [location])
+  // if the user redirect back after he submit an answer then show the results
+  useEffect(() => {
+    if (location.state !== null && "showResults" in location.state) {
+      setShowResults(true);
+    }
+  }, [location]);
 
-// show the results whether the user didn't answer before
-// or after user submit an answer
-  return (question &&
-  
-  (question.id in authUser[0].answers || ShowResults || alreadyAnswered === undefined) ? (
+  if (!question) return <Navigate to="/notfound" />;
+
+  return question.id in authUser.answers ||
+    ShowResults ||
+    alreadyAnswered === undefined ? (
     <QuestionResults />
   ) : (
     <AnswerQuestion question={question} />
-  )
-  )
+  );
 };
